a2ee8cc10acc37c22bab918fa54d7587ea75899f
thebbapp <248134211+thebbapp@users.noreply.github.com>
2025-12-23 03:17:16 +0900

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,95 @@
+// swift-tools-version: 6.2.0
+import PackageDescription
+
+let package = Package(
+    name: "BbAppAuthProviderPlaintext",
+    defaultLocalization: "en",
+    platforms: [.iOS("18.5"), .macOS("15.5"), .tvOS("18.0")],
+    products: [
+        .library(name: "BbAppAuthProviderPlaintext", targets: ["BbAppAuthProviderPlaintext"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-auth-provider.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-content-store.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-content-source.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-prompt.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-configuration.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-module-set.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-task.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-content-schema.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-intent.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-author-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-content-source-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-http-client.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-new-post-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-new-comment-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-edit-post-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-edit-comment-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-search-schema.git",
+            from: "0.1.0"
+        ),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-relative-date.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-push.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-status-bar.git",
+            from: "0.1.0"
+        ),
+        .package(url: "https://github.com/thebbapp/swift-package-bb-app-state.git", from: "0.1.0"),
+        .package(
+            url: "https://github.com/thebbapp/swift-package-bb-app-auth-provider-schema.git",
+            from: "0.1.0"
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BbAppAuthProviderPlaintext",
+            url:
+                "https://github.com/thebbapp/swift-package-bb-app-auth-provider-plaintext/releases/download/v0.1.0/BbAppAuthProviderPlaintext.xcframework.zip",
+            checksum: "5cf92d5aa6cb23c8da7843a040e80746919b70df7357c16f93b9da453d7326d2"
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ README.md
@@ -0,0 +1,9 @@
+# ``BbAppAuthProviderPlaintext``
+
+A plaintext username/password authentication provider implementation.
+
+## Overview
+
+The BbAppAuthProviderPlaintext library provides a concrete implementation of the authentication provider protocol using secure username and password credentials. This module includes the provider logic and its associated view components, offering a straightforward authentication method for applications that support basic credential-based login.
+
+## To keep BbApp development going, [donate here](https://thebbapp.com/donate)
